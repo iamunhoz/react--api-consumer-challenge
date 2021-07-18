ec87d781a101c5c950917ea979ef7961ead3e0ad
Ivã Munhoz <iamunhoz@gmail.com>
2021-07-18 15:31:00 -0300

    TopBar styled

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function TopBar() {
   return (<>
     <div className={'topbar-container'}>
-      <img src='/logo.svg'></img>
-      <div>
-        <span>Nome do Candidato</span>
+      <img className={'vertical-align topbar-logo'} src='/logo.svg'></img>
+      <div className={'vertical-align topbar-right'}>
+        <span className={'topbar-description'}>Nome do Candidato</span>
         <span>Ivã A. Munhoz</span>
-        <button>CB</button>
+        <button className={'topbar-bold topbar-button'}>CB</button>
       </div>
     </div>
     <style>{`
@@ -17,6 +17,43 @@ export default function TopBar() {
         background-color: #fff;
         display: flex;
         justify-content: space-between;
+        padding-left: 24px;
+      }
+
+      .topbar-logo {
+        width: 96px;
+        height: 24px;
+      }
+
+      .vertical-align {
+        height: 50%;
+        margin: auto 0;
+      }
+
+      .topbar-right {
+        display: flex;
+        align-items: center;
+      }
+
+      
+      .topbar-right > * {
+        margin-right: 5px;
+        font-family: 'PT Sans Caption', sans-serif;
+        color: #555555;
+      }
+      
+      .topbar-description {
+        font-weight: bold;
+      }
+
+      .topbar-button {
+        font-size: 15px;
+        font-weight: bold;
+        border: 1px solid #ddd;
+        background: #F5F5F5 0% 0% no-repeat padding-box;
+        border-radius: 4px;
+        opacity: 1;
+
       }
     `}</style></>
   )
