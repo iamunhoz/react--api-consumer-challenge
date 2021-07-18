132d0b34872cf528bbf32b6bb75061962c60093c
Ivã Munhoz <iamunhoz@gmail.com>
2021-07-18 09:33:06 -0300

    limit solved

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -5,7 +5,8 @@ const requestFromMarvel = async (resource:string, options?:string|undefined|null
   const timeStamp = Date.now()
   const hash = md5(timeStamp.toString() + PRIVATE_KEY + PUBLIC_KEY)
   const server = 'http://gateway.marvel.com/v1/public/'
-  const URL = () => `${server}${resource}?ts=${timeStamp}&apikey=${PUBLIC_KEY}&hash=${hash}${options}`
+  const URL = () => `${server}${resource}?ts=${timeStamp}&limit=100&apikey=${PUBLIC_KEY}&hash=${hash}${options}`
+  console.log(`requisition param: ${URL()}`)
   
   try {
     const response = await fetch(URL(), {
